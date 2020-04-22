a84f00715978b08fef57e008fb850b64dce1e384
Prateek Jain <prateek.jain23995@yahoo.com>
2020-04-23 00:40:51 +0800

    upating project configurations

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lein-githooks "0.1.2-SNAPSHOT"
+(defproject jainsahab/lein-githooks "1.0.0"
   :description "Leiningen plugin for managing git client hooks"
-  :url "http://github.com/gmorpheme/lein-githooks"
+  :url "https://github.com/jainsahab/lein-githooks"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
 
-  :signing {:gpg-key "github@gmorpheme.net"}
-  
-  :profiles {:dev {:plugins [[lein-githooks "0.1.0"]]
+  :signing {:gpg-key "prateek.jain23995@yahoo.com"}
+
+  :profiles {:dev {:plugins  [[lein-githooks "0.1.0"]]
                    :githooks {:pre-push ["lein test"]}}})
